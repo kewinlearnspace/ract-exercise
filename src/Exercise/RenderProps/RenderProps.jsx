a16c022f3ea3fb31b347ee19b7d7ea6f43def772
JTPeng <404502753@qq.com>
2020-07-29 09:37:39 +0800

    RenderProps  modify

--- src/Exercise/RenderProps/RenderProps.jsx
+++ src/Exercise/RenderProps/RenderProps.jsx
@@ -1,46 +1,110 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent, memo } from 'react';
 
-class Cat extends Component {
-  render() {
-    const mouse = this.props.mouse;
-    return <p style={{ position: 'absolute', left: mouse.x, top: mouse.y }}>鼠标移动部分</p>;
-  }
-}
-
-class Mouse extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { x: 0, y: 0 };
-    this.handleMouseMove = this.handleMouseMove.bind(this);
-  }
-
-  handleMouseMove(event) {
-    this.setState({
-      x: event.clientX,
-      y: event.clientY,
-    });
-  }
-  render() {
-    return (
-      <div style={{ height: '100%' }} onMouseMove={this.handleMouseMove}>
-        {this.props.render(this.state)}
-      </div>
-    );
-  }
-}
+// class Foo extends Component {
+//   // 通过比较下一次的props是否相等,判断是否要重新渲染,false 标识不需要重新渲染,true 表示需要重新渲染
+//   shouldComponentUpdate(nextProps, nextState) {
+//     console.log(nextProps, nextState);
+//     if (nextProps.name === this.props.name) {
+//       return false;
+//     }
+//     return true;
+//   }
+//   render() {
+//     console.log('Foo render');
+//     return null;
+//   }
+// }
+// class App extends Component {
+//   state = {
+//     count: 0,
+//     person: {
+//       age: 0,
+//     },
+//   };
+//   render() {
+//     const { person } = this.state;
+//     return (
+//       <div>
+//         <button
+//           type="button"
+//           onClick={() => {
+//             person.age++;
+//             this.setState({ count: this.state.count + 1 });
+//           }}
+//         >
+//           addClick
+//         </button>
+//         <Foo person={person}></Foo>
+//       </div>
+//     );
+//   }
+// }
 
 /**
- * 理解:Render Props就是为父组件的props添加一个返回组件的函数,并在父组件render的时候执行这个函数
- * A(父组件) -> B(公共代码) -> C(需要使用公共代码的组件)  将B中公共逻辑的属性(props)作为A中组件函数的属性(props)再传递给C
- * 使用场景:日志收集,异步请求等公共逻辑
+ * 表示一个纯组件，可以用来优化 react 程序。减少 render 函数渲染的次数。提高性能。进行的是浅比较，也就是说如果是引用数据类型的数据，只会比较不是同一个地址，而不会比较这个地址里面的数据是否一致
+ * 缺陷:进行的是浅比较(只有传入的props第一级的数据发生变化才会触发组件重新渲染),可以理解成可以比较出简单的数据类型是否发生变化,单无法检查到引用数据类型是否发生变化,对引用数据类型的仅是比较其指针所指向的地址是否发生变化,若是使用该方法检查引用数据类型是否发生变化,只能在其内部数据发生变化时,强行改变原来指针的指向,使其指向一个新的地址
+ * 使用场景：纯展示组件上
  */
+// 类声明组件
+// class Foo extends PureComponent {
+//   render() {
+//     console.log('Foo render');
+//     return <div>{this.props.person.age}</div>;
+//   }
+// }
+// 函数组件 - 无状态组件
+// memo使用memo包裹一层,等价于PureComponent
+// const Foo = memo(function Foo(props) {
+//   console.log('Foo render');
+//   return <div>{props.person.age}</div>;
+// });
+
+function Foo(props) {
+  console.log('Foo render');
+  return <div>{props.person.age}</div>;
+}
+
+function Foos(props) {
+  console.log('Foo render');
+  return <div>{props.person.age}</div>;
+}
+
+function updateFoos(prevProps, nextProps) {
+  console.log(prevProps, nextProps);
+}
+const NewFoo = memo(Foo);
+// const NewFoo = memo(Foos, updateFoos);
+
 class App extends Component {
+  state = {
+    person: {
+      age: 0,
+    },
+    count: 0,
+  };
+  // callback() {
+  //   // this指向
+  // }
+  callback = () => {};
   render() {
+    const { person } = this.state;
     return (
       <div>
-        <h1>Move the mouse around!</h1>
-        {/* 父组件的返回组件函数 */}
-        <Mouse render={(mouse) => <Cat mouse={mouse} />} />
+        <button
+          type="button"
+          onClick={() => {
+            person.age++;
+            this.setState({ count: this.state.count + 1, person });
+          }}
+        >
+          addClick
+        </button>
+        {/* this指向有问题 */}
+        {/* <Foo person={person} cb={this.callback}></Foo> */}
+        {/* 每次都会刷新 */}
+        {/* <Foo person={person} cb={() => {}}></Foo> */}
+        {/* <Foo person={person} cb={this.callback}></Foo> */}
+        <NewFoo person={person} cb={this.callback}></NewFoo>
       </div>
     );
   }
